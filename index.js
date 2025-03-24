aa5553df24d0714d6951cf179277f09d04f97a98
priya shukla <priyashukla22@navgurukul.org>
2025-03-24 18:54:31 +0530

    solve conflict

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-// import express from "express"
-// import mongoose from "mongoose"
-// import dotenv from "dotenv";
-// import cors from "cors"
-// dotenv.config(); 
-// const app = express();
-// const port = process.env.PORT;
-// import courseRouter from "./Routes/Route"
-
-// const mongoURI = process.env.MONGODB_URL;
-// mongoose.connect(mongoURI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// }).then(() => {
-//     console.log("MongoDB Connected Successfully!");
-
-// }).catch((err) => {
-//     console.error("MongoDB Connection Error:", err);
-// });
-
-// app.get("/", function (req, resp) {
-//     resp.send("home");
-// });
-
-// app.listen(port, () => {
-//     console.log(`Server running on the port ${port}`);
-// });
-
-
 
 import express from "express";
 import mongoose from "mongoose";
@@ -50,13 +21,12 @@ mongoose.connect(mongoURI, {
     console.error("MongoDB Connection Error:", err);
 });
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
+
 app.use("/api/courses", courseRouter);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`)
 });

1fc6250945d7807b10a76b39edcc959725ef989d
priya shukla <priyashukla22@navgurukul.org>
2025-03-24 06:51:57 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv";
+dotenv.config(); 
+const app = express();
+
+const port = process.env.PORT;
+
+
+
+const mongoURI = process.env.MONGODB_URL;
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("MongoDB Connected Successfully!");
+
+}).catch((err) => {
+    console.error("MongoDB Connection Error:", err);
+});
+
+app.get("/", function (req, resp) {
+    resp.send("home");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on the port ${port}`);
+});
